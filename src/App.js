e40bbea3950a5dcd91af58b3688633aea32e6647
DURWANKUR 10 <you@example.com>
2025-02-18 11:55:06 +0530

    Pranav-XT V 25.02.18

--- src/App.js
+++ src/App.js
@@ -94,6 +94,9 @@ function App() {
           <p>&copy; 2025 Capture Moments. All Rights Reserved. <a href="#privacy-policy">Privacy Policy</a></p>
         </div>
       </footer>
+      <a href="https://wa.me/+917757984048" target="_blank" class="whatsapp-icon">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg" alt="WhatsApp"/>
+    </a>
     </div>
   );
 }
