9a302faaca582b87a27445ba6175133398614f61
santoshpatil19955 <srpatil1603@gmail.com>
2025-02-24 15:52:00 +0530

    Final Changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -26,6 +26,7 @@ const Footer = () => {
             <a href="https://instagram.com" target="_blank"><i className="fab fa-instagram"></i></a>
             <a href="https://facebook.com" target="_blank"><i className="fab fa-facebook"></i></a>
             <a href="https://youtube.com" target="_blank"><i className="fab fa-youtube"></i></a>
+            <a href="https://twitter.com" target="_blank"><i class="fa-brands fa-twitter"></i></a>
            
           </div>  
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,10 @@ import logo from "../assets/logo.png";
 import enquireIcon from "../assets/Enquire.png"; // Import Enquire Image
 
 const Header = () => {
-  const location = useLocation();
-  const isHomePage = location.pathname === "/"; 
+  const location = useLocation(); // Get the current route
 
   return (
-    <header className={isHomePage ? "transparent-navbar" : "solid-navbar"}>
+    <header className={location.pathname === "/" ? "transparent-navbar" : "solid-navbar"}>
       <div className="logo">
         <Link to="/">
           <img src={logo} alt="Logo" />
@@ -16,21 +15,30 @@ const Header = () => {
       </div>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/service">Services</Link></li>
-          {/* <li><Link to="/contact">Contact</Link></li> */}
-          <li><Link to="/gallery">Gallery</Link></li>
-          {/* <li><Link to="/FAQ">FAQs</Link></li>
-          <li><Link to="/blog">Blog</Link></li> */}
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/cart">Cart</Link></li>
+          <li>
+            <Link to="/" className={location.pathname === "/" ? "active-link" : ""}>Home</Link>
+          </li>
+          <li>
+            <Link to="/service" className={location.pathname.startsWith("/service") ? "active-link" : ""}>Services</Link>
+          </li>
+          <li>
+            <Link to="/gallery" className={location.pathname.startsWith("/gallery") ? "active-link" : ""}>Gallery</Link>
+          </li>
+          <li>
+            <Link to="/about" className={location.pathname.startsWith("/about") ? "active-link" : ""}>About</Link>
+          </li>
+          <li>
+            <Link to="/cart" className={location.pathname.startsWith("/cart") ? "active-link" : ""}>Cart</Link>
+          </li>
         </ul>
       </nav>
 
       {/* Enquire Now Section */}
       <div className="enquire-now">
         <img src={enquireIcon} alt="Enquire" />
-        <span><Link to="/Contact">Enquire Now</Link></span>
+        <span>
+          <Link to="/contact" className={location.pathname.startsWith("/contact") ? "active-link" : ""}>Enquire Now</Link>
+        </span>
       </div>
     </header>
   );
